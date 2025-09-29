08fc47cf57654f3fcafccc896bbe4221092d7c36
Victor <Victorfoliveira0407@gmail.com>
2025-09-29 09:52:03 -0300

    Status codes

--- src/controllers/bruxosController.js
+++ src/controllers/bruxosController.js
@@ -1,10 +1,14 @@
+import { request } from "express";
 import dados from "../models/data.js";
 const {bruxos} = dados;
 
 const getAllBruxos = (req, res) => {
     res.status(200).json({
+        status: 200,
+        success: true,
+        message: "Bruxos encontrados com sucesso",
         total: bruxos.length,
-        bruxos: bruxos
+        data: bruxos
     });
 }
 
@@ -14,22 +18,76 @@ const getBruxoById = (req, res) => {
     const bruxo = bruxos.find(b => b.id === id);
 
     if (!bruxo) {
-        res.status(400);
-    }
-
-    res.status(200).json({
-        total: bruxo.length,
-        bruxo: bruxo
+        res.status(404).json({
+            status: 404,
+            error: "Not founded",
+            success: false,
+            message: `Bruxo com o id ${id} não encontrado`,
+            data: null
+        });;
+    } else {
+        res.status(200).json({
+            status: 200,
+            success: true,
+            message: `Bruxo com o id ${id} encontrado`,
+            total: bruxo.length,
+            data: bruxo
     });
+    }
 }
 
 const createBruxo = (req, res) => {
     const {nome, casa, anoNascimento, especialidade, nivelMagia, ativo} = req.body;
+    const nomeExiste = bruxos.some(n => n.nome.toLowerCase() === nome.toLowerCase());
 
-    if (!nome || !casa) {
+    const {admin} = req.body;
+
+    if (!admin) {
+        return res.status(403).json({
+            status: 403,
+            error: "Forbidden",
+            success: false,
+            message: "Você não possui permissão para criar um bruxo"
+        });
+    }
+
+    if (!nome) {
         return res.status(400).json({
+            status: 400,
+            error: "Bad request",
+            success: false,
+            message: "Requisição mal executada! 'nome' é obrigatório",
+            data: null
+        });
+    }
+    
+    if (nomeExiste) {
+        return res.status(409).json({
+            status: 409,
+            error: "Conflict",
+            success: false,
+            message: `Bruxo com o nome ${nome} já existe`,
+            data: null
+        });
+    }
+
+    if (!casa) {
+        return res.status(400).json({
+            status: 400,
+            error: "Bad request",
             sucess: false,
-            message: "Nome e casa são obrigatórios"
+            message: "Requisição mal executada! 'casa' é obrigatória",
+            data: null
+        });
+    }
+
+    if (!anoNascimento) {
+        return res.status(400).json({
+            status: 400,
+            error: "Bad request",
+            success: false,
+            message: "Requisição mal executada! 'anoNascimento' é obrigatório",
+            data: null
         });
     }
 
@@ -46,19 +104,94 @@ const createBruxo = (req, res) => {
     bruxos.push(novoBruxo);
 
     res.status(201).json({
-        sucess: true,
+        status: 201,
+        success: true,
         message: "Bruxo cadastrado com sucesso",
-        bruxo: novoBruxo
+        data: novoBruxo
+    });
+}
+
+const updateBruxo = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {nome, casa, anoNascimento, especialidade, nivelMagia, ativo} = req.body;
+
+    const {admin} = req.body;
+
+    if (!admin) {
+        return res.status(403).json({
+            status: 403,
+            error: "Forbidden",
+            success: false,
+            message: "Você não possui permissão para atualizar um bruxo"
+        });
+    }
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            status: 400,
+            error: "Bad request",
+            success: false,
+            message: "O id deve ser válido",
+            data: null
+        });
+    }
+
+    const bruxoExiste = personagens.find(b => b.id === id);
+
+    if (!bruxoExiste) {
+        return res.status(404).json({
+            status: 404,
+            error: "Not founded",
+            success: false,
+            message: "Este bruxo não existe",
+            data: null
+        });
+    }
+
+    const bruxoAtualizado = bruxos.map(bruxo => bruxo.id === id
+        ? {
+            ...bruxo,
+            ...(nome && {nome}),
+            ...(casa && {casa}),
+            ...(anoNascimento && {anoNascimento}),
+            ...(especialidade && {especialidade}),
+            ...(nivelMagia && {nivelMagia}),
+            ...(ativo && {ativo})
+        }
+        : bruxo
+    );
+
+    bruxos.splice(0, bruxos.length, ...bruxoAtualizado);
+
+    const bruxoAtualizados = bruxos.find(p => p.id === id);
+
+    res.status(200).json({
+        status: 200,
+        success: true,
+        message: "Bruxo atualizado com sucesso",
+        data: bruxoAtualizados
     });
 }
 
 const deleteBruxo = (req, res) => {
-    let id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
+    const {admin} = req.body;
+
+    if (!admin) {
+        return res.status(403).json({
+            status: 403,
+            error: "Forbidden",
+            success: false,
+            message: "Você não possui permissão para deletar um bruxo"
+        });
+    }
+
 
     if (isNaN(id)) {
         return res.status(400).json({
-            sucess: false,
-            message: "O ID deve ser válido"
+            status: 400,
+            success: false,
+            message: "O id deve ser válido"
         });
     }
 
@@ -66,19 +199,20 @@ const deleteBruxo = (req, res) => {
 
     if(!bruxoParaRemover) {
         return res.status(404).json({
-            sucess: false,
-            message: `Bruxo com o id ${id} não existe`
+            status: 404,
+            success: false,
+            message: `Bruxo com o id ${id} não encontrado`
         });
     }
-
     const bruxosFiltrados = bruxos.filter(bruxo => bruxo.id !== id);
 
     bruxos.splice(0, bruxos.length, ...bruxosFiltrados);
 
     res.status(200).json({
-        sucess: true,
+        status: 200,
+        success: true,
         message: `O bruxo com id ${id} foi removido com sucesso`
     });
 }
 
-export {getAllBruxos, getBruxoById, createBruxo, deleteBruxo};
+export {getAllBruxos, getBruxoById, createBruxo, updateBruxo, deleteBruxo};
--- src/routes/bruxosRoutes.js
+++ src/routes/bruxosRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {getAllBruxos, getBruxoById, createBruxo, deleteBruxo} from "../controllers/bruxosController.js";
+import {getAllBruxos, getBruxoById, createBruxo, deleteBruxo, updateBruxo} from "../controllers/bruxosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllBruxos);
 router.get("/id/:id", getBruxoById);
 router.post("/", createBruxo);
+router.put("/id/:id", updateBruxo);
 router.delete("/id/:id", deleteBruxo);
 
 export default router;
